4dd4c4588c462ffb927233657f12dfd63434888e
alivadjid <mshagidullin@rzdit.ru>
2023-01-11 23:00:34 +0300

    change

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import typescript2 from "rollup-plugin-typescript2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [
     vue(),
     typescript2({
